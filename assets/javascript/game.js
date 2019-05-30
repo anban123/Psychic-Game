cb39580a648584d8c7367edbe7b641906c1b5861
Anna Grant <anban123@msn.com>
2019-05-30 16:46:43 -0600

    fixed some link issues but broke a few other things

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,29 +2,33 @@ var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 
 var wins = 0;
 var losses = 0;
+// var guessesLeft = 10;
 
 var directionstext = document.getElementById("directions-text");
 var winstext = document.getElementById("wins-text");
 var lossestext = document.getElementById("losses-text");
-var guessesLefttext = document.getElementById("guessesLeft-text");
+// var guessesLefttext = document.getElementById("guessesLeft-text");
 var userGuesstext = document.getElementById("userGuess-text");
 
 document.onkeyup = function(event) {
     var userGuess = event.key;
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-    if (userGuess === computerChoices) {
-        wins++;
-    } else if (userGuess !== computerChoices) {
+    if (userGuess === computerGuess) {
+        wins++; 
+    } else if (userGuess !== computerGuess) {
         losses++
+
     } else {
         alert("WHAT DID YOU DO??!!!!!")
     }
 
 
-    winstext.textcontent = wins;
-    lossestext.textcontent = losses;
-    // guessesLefttext =  ;
-    userGuesstext = userGuess;
+
+
+    winstext.textcontent = "Wins: " + wins;
+    lossestext.textcontent = "Losses: " + losses;
+    // guessesLefttext = "Guesses Left: ";                           //NEED + ...
+    userGuesstext = "Your Guesses so far: " + userGuess;
 
 }
